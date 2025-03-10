fbc1c0f6a955aa195a8e97c33f888d2d462cb84c
aelaraby6 <body1262005@gmail.com>
2025-03-10 18:53:11 +0200

    add bookmodel

--- src/app.js
+++ src/app.js
@@ -5,14 +5,22 @@ const PORT = 3000
 
 const app = express()
 
-app.listen(PORT,() =>{
-    console.log("hello");
-})
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
 
 // middelewares
-
 app.use(express.json())
+
+
+// connect DB
+
+mongoose.connect(
+  "",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Connection error:", err));
+
+
+app.listen(PORT,() =>{
+    console.log("server is start on port 3000");
+})
+
---
+++ src/routes/bookRoutes.js
@@ -0,0 +1,7 @@
+import express from "express";
+import { Router } from "express";
+import Book from "../models/bookModel"
+
+
+const router = express.Router()
+
