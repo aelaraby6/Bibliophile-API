44d745fc408c0bfcda4117da211a8a7483f11086
aelaraby6 <body1262005@gmail.com>
2025-03-21 15:53:52 +0200

    add user model

---
+++ src/Services/passwordService.js
@@ -0,0 +1,14 @@
+import bcrypt from "bcrypt"
+
+const saltRounds = 10;
+
+async function hashPassword(password) {
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    return hashPassword;
+}
+
+
+async function comparePassword(password, hashedPassword) {
+    const match = await bcrypt.compare(password, hashedPassword);
+    return match;
+}
